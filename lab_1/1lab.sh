4f5a24a40e2a74a5b26dc579df317c890078af59
Sergey Samoilenko <darksteel37@gmail.com>
2019-09-11 13:09:53 +0700

    90% Done!

--- lab_1/1lab.sh
+++ lab_1/1lab.sh
@@ -10,6 +10,13 @@ PROC_GHz=$(lscpu | grep -i "CPU МГц:")
 PROC_THREADS=$(lscpu | grep -i "Потоков на ядро:")
 MEM_ALL=$(cat /proc/meminfo | grep "MemTotal:")
 MEM_FREE=$(cat /proc/meminfo | grep "MemFree:")
+#HDD_TOTAL=$(lsblk | grep "sda1" | awk '{print $4}')
+HDD_TOTAL=$(df -h | grep "/dev/sda1" | awk '{print $2}')
+HDD_FREE=$(df -h | grep "/dev/sda1" | awk '{print $4}')
+SWAP_TOTAL=$(swapon -s | grep "/swapfile" | awk '{print $3}')
+SWAP_USED=$(swapon -s | grep "/swapfile" | awk '{print $4}')
+SWAP_FREE=$((SWAP_TOTAL - SWAP_USED))
+
 
 echo "Дата:"
 echo "	$(date)"
@@ -27,5 +34,9 @@ echo "Оперативная память:"
 echo "	$MEM_ALL"
 echo "	$MEM_FREE"
 echo "Жесткий диск:"
-
-
+echo "	Всего:	$HDD_TOTAL"
+echo "	Доступно $HDD_FREE"
+echo "	Смонтировано в корневую папку / - $HDD_TOTAL"
+echo " 	SWAP всего - $SWAP_TOTAL"
+echo "	SWAP доступно - $SWAP_FREE"
+echo "Сетевые интерфейсы:"
